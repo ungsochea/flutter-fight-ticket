a4a19004d2ef9588ac606ebd3c2bc782b4073c44
ungsochea <sochea.ung@gmail.com>
2019-05-17 13:35:21 +0700

    search bar, option selected

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class HomeScreenTopPart extends StatefulWidget {
 
 class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
   var selectedLocationIndex = 0;
+  var isFlightSelected = true;
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +75,7 @@ class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
                         width: 16.0,
                       ),
                       PopupMenuButton(
-                        onSelected: (index){
+                        onSelected: (index) {
                           setState(() {
                             selectedLocationIndex = index;
                           });
@@ -109,14 +110,124 @@ class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
                               )
                             ],
                       ),
+                      Spacer(),
+                      Icon(Icons.settings, color: Colors.white),
                     ],
                   ),
-                )
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                Text(
+                  'Where world\nyou want to go?',
+                  style: TextStyle(fontSize: 24.0, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 32.0),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30.0),
+                    ),
+                    child: TextField(
+                      controller: TextEditingController(text: locations[0]),
+                      style: dropDownMenuItemStyle,
+                      cursorColor: appTheme.primaryColor,
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 32.0, vertical: 14.0),
+                          suffixIcon: Material(
+                            elevation: 2.0,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30.0)),
+                            child: Icon(Icons.search, color: Colors.black),
+                          ),
+                          border: InputBorder.none),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    InkWell(
+                      child: ChoiceChip(
+                          Icons.flight_takeoff, "Floghts", isFlightSelected),
+                      onTap: () {
+                        setState(() {
+                          isFlightSelected = true;
+                        });
+                      },
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    InkWell(
+                      child: ChoiceChip(Icons.hotel, "Hotels", !isFlightSelected),
+                      onTap: () {
+                        setState(() {
+                          isFlightSelected = false;
+                        });
+                      },
+                    ),
+                    
+                  ],
+                ),
               ],
             ),
           ),
         )
       ],
+    );
+  }
+}
+
+class ChoiceChip extends StatefulWidget {
+  final IconData icon;
+  final String text;
+  final bool isSelected;
+
+  ChoiceChip(this.icon, this.text, this.isSelected);
+
+  @override
+  _ChoiceChipState createState() => _ChoiceChipState();
+}
+
+class _ChoiceChipState extends State<ChoiceChip> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 8.0),
+      decoration: widget.isSelected
+          ? BoxDecoration(
+              color: Colors.white.withOpacity(0.15),
+              borderRadius: BorderRadius.all(Radius.circular(20.0)))
+          : null,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Icon(
+            widget.icon,
+            size: 20,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 8.0,
+          ),
+          Text(
+            widget.text,
+            style: TextStyle(color: Colors.white, fontSize: 16.0),
+          )
+        ],
+      ),
     );
   }
 }
