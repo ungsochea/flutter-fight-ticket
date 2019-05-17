1220fbff14fddb92f3f25ccd3c9363fcdb8c941e
ungsochea <sochea.ung@gmail.com>
2019-05-17 09:01:14 +0700

    dropdownitem

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,34 @@ void main() => runApp(MaterialApp(
       title: 'Flight List Mock Up',
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
+      theme: appTheme,
     ));
+
+Color firstColor = Color(0xFFF47D15);
+Color secondColor = Color(0xFFEF772C);
+
+ThemeData appTheme = ThemeData(
+  primaryColor: Color(0xFFF3791A),
+  fontFamily: 'Oxygen',
+);
+
+List<String> locations = ['Phnom Penh (PP)', 'Siam Reap (SR)'];
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: <Widget>[
-          HomeScreenTopPart()
-        ],
+        children: <Widget>[HomeScreenTopPart()],
       ),
     );
   }
 }
+
+const TextStyle dropDownLabelStyle =
+    TextStyle(color: Colors.white, fontSize: 16.0);
+const TextStyle dropDownMenuItemStyle =
+    TextStyle(color: Colors.black, fontSize: 16.0);
 
 class HomeScreenTopPart extends StatefulWidget {
   @override
@@ -26,6 +40,8 @@ class HomeScreenTopPart extends StatefulWidget {
 }
 
 class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
+  var selectedLocationIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -35,7 +51,69 @@ class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
           clipper: CustomShapeClipper(),
           child: Container(
             height: 400.0,
-            color: Colors.orange,
+            // color: Colors.orange,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [firstColor, secondColor],
+              ),
+            ),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 50.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.location_on,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 16.0,
+                      ),
+                      PopupMenuButton(
+                        onSelected: (index){
+                          setState(() {
+                            selectedLocationIndex = index;
+                          });
+                        },
+                        child: Row(
+                          children: <Widget>[
+                            Text(
+                              locations[selectedLocationIndex],
+                              style: dropDownLabelStyle,
+                            ),
+                            Icon(
+                              Icons.keyboard_arrow_down,
+                              color: Colors.white,
+                            )
+                          ],
+                        ),
+                        itemBuilder: (BuildContext context) =>
+                            <PopupMenuItem<int>>[
+                              PopupMenuItem(
+                                child: Text(
+                                  locations[0],
+                                  style: dropDownMenuItemStyle,
+                                ),
+                                value: 0,
+                              ),
+                              PopupMenuItem(
+                                child: Text(
+                                  locations[1],
+                                  style: dropDownMenuItemStyle,
+                                ),
+                                value: 1,
+                              )
+                            ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         )
       ],
